c41b105557ad418183e6c0fb53fade40620ad3f1
Joy Dutta <dutta_joy@hotmail.com>
2023-12-02 22:08:49 +0000

    Section 2 - Input Tag Helpers

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,9 +1,15 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel;
+
+namespace BulkyWeb.Models
 {
     public class Category
     {
         public int Id { get; set; }
+
+        
         public string Name { get; set; }
+
+        //[DisplayName("Display Order")]  example if asp-for was used
         public int DisplayOrder { get; set; }
     }
 }
