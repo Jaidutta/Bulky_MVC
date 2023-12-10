1374572a238a470b42d4707873a93182cf529a2f
Joy Dutta <dutta_joy@hotmail.com>
2023-12-10 05:21:50 +0000

    Section 2 - Server Side Validations

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -5,29 +5,38 @@ using Microsoft.AspNetCore.Mvc;
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
-    {   
+    {
         private readonly ApplicationDbContext _db;
         public CategoryController(ApplicationDbContext db)
         {
             _db = db;
         }
         public IActionResult Index()
-        {   
+        {
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
 
         public IActionResult Create()
         {
-           
+
             return View();
         }
 
         [HttpPost]
         public IActionResult Create(Category obj)
-        {   _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index", "Category");
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index", "Category");
+            } else
+            {
+                return View();
+            }
+            
         }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,15 +1,19 @@
-﻿using System.ComponentModel;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
 
-        
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
 
         //[DisplayName("Display Order")]  example if asp-for was used
+
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]    
         public int DisplayOrder { get; set; }
     }
 }
