367acedf8d5dce026913a4ad2b794550b468c666
Joy Dutta <dutta_joy@hotmail.com>
2023-12-13 03:18:06 +0000

    Section 2 - Custom Validations

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -26,6 +26,10 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Category Name");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
